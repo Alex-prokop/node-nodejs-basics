cc4ed87eb35db14633effa16ea58809633c44661
Alexandr Prokopovich <prokop7@mail.ru>
2025-10-26 19:36:52 +0300

    add: create.js function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import { promises as fs } from 'fs';
+import { join } from 'path';
+
 const create = async () => {
-  // Write your code here
+  const filesDir = join(process.cwd(), 'src', 'fs', 'files');
+  const freshFile = join(filesDir, 'fresh.txt');
+  const fileContent = 'I am fresh and young';
+
+  try {
+    await fs.mkdir(filesDir, { recursive: true });
+
+    const fileHandle = await fs.open(freshFile, 'wx');
+    await fileHandle.write(fileContent);
+    await fileHandle.close();
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await create();
