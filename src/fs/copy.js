d3147b9d416ea4aa5ce4146906e8800c77063516
Alexandr Prokopovich <prokop7@mail.ru>
2025-10-26 22:12:05 +0300

    feat(fs): implement copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,27 @@
+import { promises as fs } from 'fs';
+import { join } from 'path';
+import { getEsmPaths } from './getEsmPaths.js';
+
+const { __dirname } = getEsmPaths(import.meta.url);
+
 const copy = async () => {
-  // Write your code here
+  const srcDir = join(__dirname, 'files');
+  const destDir = join(__dirname, 'files_copy');
+
+  try {
+    await fs.access(srcDir);
+
+    try {
+      await fs.access(destDir);
+      throw new Error('FS operation failed');
+    } catch (err) {
+      if (err.code !== 'ENOENT') throw err;
+    }
+
+    await fs.cp(srcDir, destDir, { recursive: true, errorOnExist: true });
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await copy();
