8a0e2a35312316d2ec9d27eb7a3f9c279147b933
Alexandr Prokopovich <prokop7@mail.ru>
2025-10-26 22:38:46 +0300

    feat(fs): implement rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,26 @@
+import { promises as fs } from 'fs';
+import { join } from 'path';
+import { getEsmPaths } from './getEsmPaths.js';
+
+const { __dirname } = getEsmPaths(import.meta.url);
+
 const rename = async () => {
-  // Write your code here
+  const wrong = join(__dirname, 'files', 'wrongFilename.txt');
+  const proper = join(__dirname, 'files', 'properFilename.md');
+
+  try {
+    await fs.access(wrong);
+    try {
+      await fs.access(proper);
+      throw new Error('FS operation failed');
+    } catch (err) {
+      if (err.code !== 'ENOENT') throw err;
+    }
+
+    await fs.rename(wrong, proper);
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await rename();
