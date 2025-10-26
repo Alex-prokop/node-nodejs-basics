2917f61c882addd61a9efacbb67e7b18411bccbf
Alexandr Prokopovich <prokop7@mail.ru>
2025-10-26 22:32:41 +0300

    feat(fs): implement delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,17 @@
+import { promises as fs } from 'fs';
+import { join } from 'path';
+import { getEsmPaths } from './getEsmPaths.js';
+
+const { __dirname } = getEsmPaths(import.meta.url);
+
 const remove = async () => {
-  // Write your code here
+  const file = join(__dirname, 'files', 'fileToRemove.txt');
+
+  try {
+    await fs.unlink(file);
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await remove();
