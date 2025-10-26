1a15164ebcbda09d6c38db61be6c7b6c722d7ffa
Alexandr Prokopovich <prokop7@mail.ru>
2025-10-27 01:18:24 +0300

    feat(zip): implement gzip compression and decompression using Streams API

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,25 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createGzip } from 'node:zlib';
+import path from 'node:path';
+import { getEsmPaths } from '../utils/getEsmPaths.js';
+
+const { __dirname } = getEsmPaths(import.meta.url);
+
 const compress = async () => {
-  // Write your code here
+  const sourcePath = path.join(__dirname, 'files', 'fileToCompress.txt');
+  const destinationPath = path.join(__dirname, 'files', 'archive.gz');
+
+  const readStream = createReadStream(sourcePath);
+  const writeStream = createWriteStream(destinationPath);
+  const gzip = createGzip();
+
+  readStream
+    .pipe(gzip)
+    .pipe(writeStream)
+    .on('finish', () => console.log('✅ Compression finished.'))
+    .on('error', () => {
+      throw new Error('FS operation failed');
+    });
 };
 
 await compress();
