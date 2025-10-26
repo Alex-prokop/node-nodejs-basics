d5559eb149e1b9887ab327f4d0932bb439eb65f0
Alexandr Prokopovich <prokop7@mail.ru>
2025-10-26 23:01:47 +0300

    feat(fs): implement list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,18 @@
+import { promises as fs } from 'fs';
+import { join } from 'path';
+import { getEsmPaths } from './getEsmPaths.js';
+
+const { __dirname } = getEsmPaths(import.meta.url);
+
+
 const list = async () => {
-  // Write your code here
-};
+  const filesDir = join(__dirname, 'files');
+
+  try {
+    const files = await fs.readdir(filesDir);
+    console.log(files);
+  } catch {
+    throw new Error('FS operation failed');
+  }};
 
 await list();
