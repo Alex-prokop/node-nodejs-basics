5596bf793e03e7c3c931add739fa6f8b012bf299
Alexandr Prokopovich <prokop7@mail.ru>
2025-10-26 22:56:55 +0300

    feat(fs): implement read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,18 @@
+import { promises as fs } from 'fs';
+import { join } from 'path';
+import { getEsmPaths } from './getEsmPaths.js';
+
+const { __dirname } = getEsmPaths(import.meta.url);
+
 const read = async () => {
-  // Write your code here
+  const filePath = join(__dirname, 'files', 'fileToRead.txt');
+
+  try {
+    const data = await fs.readFile(filePath, 'utf-8');
+    console.log(data);
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await read();
